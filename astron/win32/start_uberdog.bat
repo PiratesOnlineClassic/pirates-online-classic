d733bd7343e16f5660392466cabec39d84c937c7
Jordan Maxwell <jordanmax@nxt-studios.com>
2018-04-03 20:13:35 -0500

    astron/win32: Prepared batch files for Astron

---
+++ astron/win32/start_uberdog.bat
@@ -0,0 +1,16 @@
+@echo off
+title Pirates Online Classic - UberDOG
+cd ../../
+
+rem Choose correct python command to execute the game
+set PYTHON_CMD=ppythona
+
+echo ============================================
+echo Starting Pirates Online Classic UberDOG...
+echo PPython: %PYTHON_CMD%
+echo ============================================
+
+rem Start UberDOG server
+:main
+%PYTHON_CMD% -m pirates.uberdog.ServiceStart 
+goto main
