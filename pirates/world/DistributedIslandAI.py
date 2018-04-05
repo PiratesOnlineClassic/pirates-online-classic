ce7fb743743d89faf16f51ea8b12a9d6527f8640
Bob <mlgrainbowluna@gmail.com>
2018-04-05 16:15:29 -0600

    pirates/world: fix some visibility issues with objects

--- pirates/world/DistributedIslandAI.py
+++ pirates/world/DistributedIslandAI.py
@@ -9,8 +9,7 @@ class DistributedIslandAI(DistributedCartesianGridAI, DistributedGameAreaAI, Tea
     notify.setInfo(True)
 
     def __init__(self, air):
-        DistributedCartesianGridAI.__init__(self, air, ISLAND_GRID_STARTING_ZONE, ISLAND_GRID_SIZE,
-            ISLAND_GRID_RADIUS, ISLAND_CELL_SIZE)
+        DistributedCartesianGridAI.__init__(self, air, ISLAND_GRID_STARTING_ZONE, LARGE_ISLAND_GRID_SIZE, ISLAND_GRID_RADIUS, ISLAND_CELL_SIZE)
 
         DistributedGameAreaAI.__init__(self, air)
         Teamable.__init__(self)
