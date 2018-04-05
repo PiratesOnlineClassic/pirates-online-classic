4455b9b39aaeb7907064578b0fcc628afe8cdae2
Jordan Maxwell <jordanmax@nxt-studios.com>
2018-04-04 21:43:49 -0500

    pirates/piratesbase: Fixed time sync

--- pirates/piratesbase/DistributedTimeOfDayManagerAI.py
+++ pirates/piratesbase/DistributedTimeOfDayManagerAI.py
@@ -15,10 +15,47 @@ class DistributedTimeOfDayManagerAI(DistributedObjectAI):
         self.cycleType = TODGlobals.TOD_REGULAR_CYCLE
         self.cycleSpeed = config.GetInt('tod-cycle-speed', 1)
         self.startingNetTime = globalClockDelta.getFrameNetworkTime(bits=32)
-        self.timeOffset = time.time() % PiratesGlobals.TOD_CYCLE_DURATION
+        self.timeOffset = 0
+        self.cycleTask = None
+
+    def announceGenerate(self):
+        DistributedObjectAI.announceGenerate(self)
+        self.cycleTask = taskMgr.doMethodLater(1, self.__runCycle, self.uniqueName('runCycle'))
 
     def delete(self):
         DistributedObjectAI.delete(self)
+
+        if self.cycleTask:
+            taskMgr.remove(self.cycleTask)
+
+    def __runCycle(self, task):
+        REALSECONDS_PER_GAMEHOUR = PiratesGlobals.TOD_CYCLE_DURATION / self.cycleSpeed
+        self.timeOffset += REALSECONDS_PER_GAMEHOUR 
+
+        if self.getCurrentIngameTime() >= 24:
+            self.timeOffset = 0
+
+        return task.again
+
+    def getCurrentIngameTime(self, time=None):
+        cycleSpeed = self.cycleSpeed
+        if cycleSpeed <= 0:
+            cycleSpeed = 1
+
+        if time is None:
+            currentTime = globalClockDelta.networkToLocalTime(
+                globalClockDelta.getFrameNetworkTime(bits=32))
+        else:
+            currentTime = time
+       
+        REALSECONDS_PER_GAMEDAY = PiratesGlobals.TOD_CYCLE_DURATION / cycleSpeed
+        REALSECONDS_PER_GAMEHOUR = float(REALSECONDS_PER_GAMEDAY / 24)
+        cycleDuration = REALSECONDS_PER_GAMEHOUR * 24
+        timeElapsed = currentTime - self.startingNetTime
+        timeIntoCycle = (timeElapsed + self.timeOffset) % cycleDuration
+        hoursIntoCycle = timeIntoCycle / REALSECONDS_PER_GAMEHOUR
+        return hoursIntoCycle
+
 
     def sync(self, cycleType, cycleSpeed, startingNetTime, timeOffset):
         self.cycleType = cycleType
