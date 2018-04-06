3a958167ad78892a7e996780f6959f60c525af72
Bob <mlgrainbowluna@gmail.com>
2018-04-05 22:11:56 -0600

    pirates/pirate: fix missing import causing a crash

--- pirates/pirate/LocalPirateGameFSM.py
+++ pirates/pirate/LocalPirateGameFSM.py
@@ -1,7 +1,3 @@
-# uncompyle6 version 3.1.1
-# Python bytecode 2.4 (62061)
-# Decompiled from: Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)]
-# Embedded file name: pirates.pirate.LocalPirateGameFSM
 import random
 
 from direct.directnotify.DirectNotifyGlobal import directNotify
@@ -19,6 +15,7 @@ from pirates.piratesgui.RewardPanel import RewardPanel
 from pirates.reputation import ReputationGlobals
 from pirates.uberdog.UberDogGlobals import InventoryType
 from PlayerPirateGameFSM import PlayerPirateGameFSM
+from otp.nametag import NametagGlobals
 
 
 class LocalPirateGameFSM(PlayerPirateGameFSM):
