7d57b0712f9078c4cca11a39e5e0c1d932bfcee1
Jordan Maxwell <jordanmax@nxt-studios.com>
2018-04-10 22:37:26 -0500

    pirates/piratesbase: Fixed loading screen

--- otp/avatar/DistributedAvatar.py
+++ otp/avatar/DistributedAvatar.py
@@ -1,7 +1,3 @@
-# uncompyle6 version 3.1.1
-# Python bytecode 2.4 (62061)
-# Decompiled from: Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)]
-# Embedded file name: otp.avatar.DistributedAvatar
 import string
 import time
 
@@ -258,4 +254,3 @@ class DistributedAvatar(DistributedActor, Avatar):
 
     def confirmAvOnShard(self, avId, onShard=True):
         messenger.send('AvOnShard%s' % avId, [onShard])
-# okay decompiling .\otp\avatar\DistributedAvatar.pyc
