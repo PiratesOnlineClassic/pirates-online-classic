25ab398a138798937fa2a6d86e601883e92d6cc6
Jordan Maxwell <jordanmax@nxt-studios.com>
2018-04-14 17:10:30 -0500

    otp/distributed: Implement CentralLogger interface

--- otp/distributed/CentralLogger.py
+++ otp/distributed/CentralLogger.py
@@ -1,7 +1,3 @@
-# uncompyle6 version 3.1.1
-# Python bytecode 2.4 (62061)
-# Decompiled from: Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)]
-# Embedded file name: otp.distributed.CentralLogger
 from direct.distributed.DistributedObjectGlobal import DistributedObjectGlobal
 
 REPORT_PLAYER = 'REPORT_PLAYER'
@@ -11,7 +7,6 @@ ReportRudeBehavior = 'MODERATION_RUDE_BEHAVIOR'
 ReportBadName = 'MODERATION_BAD_NAME'
 
 class CentralLogger(DistributedObjectGlobal):
-    __module__ = __name__
     PlayersReportedThisSession = {}
 
     def hasReportedPlayer(self, targetDISLId, targetAvId):
@@ -26,4 +21,3 @@ class CentralLogger(DistributedObjectGlobal):
 
     def writeClientEvent(self, eventString):
         self.sendUpdate('sendMessage', ['ClientEvent', eventString, 0, 0])
-# okay decompiling .\otp\distributed\CentralLogger.pyc
--- otp/distributed/CentralLoggerAI.py
+++ otp/distributed/CentralLoggerAI.py
@@ -1,8 +1,36 @@
-from direct.distributed.DistributedObjectAI import DistributedObjectAI
+from direct.distributed.DistributedObjectGlobalAI import DistributedObjectGlobalAI
 from direct.directnotify import DirectNotifyGlobal
 
-class CentralLoggerAI(DistributedObjectAI):
+class CentralLoggerAI(DistributedObjectGlobalAI):
     notify = DirectNotifyGlobal.directNotify.newCategory('CentralLoggerAI')
 
     def __init__(self, air):
-        DistributedObjectAI.__init__(self, air)
+        DistributedObjectGlobalAI.__init__(self, air)
+
+    def sendMessage(self, category, message, targetDISLid, targetAvId):
+        self.sendUpdate('sendMessage', [category, message, targetDISLid, targetAvId])
+
+    def logMessageLocally(self, category, message, targetDISLid, targetAvId):
+        parts = message.split('|')
+        msgType = parts[0]
+        fields = {
+            'targetDISLid': targetDISLid,
+            'targetAvId': targetAvId
+        }
+
+        if msgType == 'GUEST_FEEDBACK':
+            fields['feedbackCategory'] = parts[1]
+            fields['feedbackMessage'] = parts[2]
+
+        if self.notify.getDebug():
+            event = {
+                'category': category,
+                'message': message,
+                'type': msgType,
+            }
+            event.update(fields)
+
+            data = json.dumps(event)
+            print(data)
+
+        self.air.writeServerEvent(category, messageType=msgType, message=message, **fields)
