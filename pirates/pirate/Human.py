1d888d5b7368cdddbd653bb4840cd017b28a38fc
Bob <mlgrainbowluna@gmail.com>
2018-04-17 22:54:56 -0600

    pirates/pirate: decompiler cleanup

--- pirates/pirate/Human.py
+++ pirates/pirate/Human.py
@@ -1,7 +1,3 @@
-# uncompyle6 version 3.1.1
-# Python bytecode 2.4 (62061)
-# Decompiled from: Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)]
-# Embedded file name: pirates.pirate.Human
 import random
 
 from direct.actor import Actor
@@ -456,4 +452,3 @@ class Human(HumanBase.HumanBase, Biped.Biped):
         cls.prebuiltAnimData[qualifier].pop('intro')
 
 Human.setupAnimDicts()
-# okay decompiling .\pirates\pirate\Human.pyc
--- pirates/pirate/HumanBase.py
+++ pirates/pirate/HumanBase.py
@@ -1,7 +1,3 @@
-# uncompyle6 version 3.1.1
-# Python bytecode 2.4 (62061)
-# Decompiled from: Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)]
-# Embedded file name: pirates.pirate.HumanBase
 import copy
 
 from pirates.pirate.HumanDNA import HumanDNA
@@ -830,4 +826,3 @@ class HumanBase:
         self.b_setClothesBelt(dna.getClothesBelt()[0], dna.getClothesBelt()[1])
         self.b_setClothesTopColor(dna.getClothesTopColor()[0], dna.getClothesTopColor()[1], dna.getClothesTopColor()[2])
         self.b_setClothesBotColor(dna.getClothesBotColor()[0], dna.getClothesBotColor()[1], dna.getClothesBotColor()[2])
-# okay decompiling .\pirates\pirate\HumanBase.pyc
