f727f8041fc87c2766bf5fe737e2fb5bd14e41c7
Jordan Maxwell <jordanmax@nxt-studios.com>
2018-04-21 22:56:44 -0500

    pirates/economy: Added shop ai

--- pirates/economy/DistributedShopKeeperAI.py
+++ pirates/economy/DistributedShopKeeperAI.py
@@ -1,5 +1,6 @@
 from direct.distributed.DistributedObjectAI import DistributedObjectAI
 from direct.directnotify import DirectNotifyGlobal
+from pirates.economy import EconomyGlobals
 
 class DistributedShopKeeperAI(DistributedObjectAI):
     notify = DirectNotifyGlobal.directNotify.newCategory('DistributedShopKeeperAI')
@@ -8,7 +9,63 @@ class DistributedShopKeeperAI(DistributedObjectAI):
         DistributedObjectAI.__init__(self, air)
         
     def requestMakeSale(self, buying, selling, names):
-        pass
+        avatar = self.air.doId2do.get(self.air.getAvatarIdFromSender())
+
+        if not avatar:
+            self.notify.warning('Failed to process make sale for non-existant avatar %d!' %
+                avatar.doId) 
+
+            return
+
+        inventory = self.air.inventoryManager.getInventory(avatar.doId)
+        if not inventory:
+            self.notify.debug('Cannot process sale for avatar %d, unknown inventory!' % 
+                avatar.doId)
+
+            return
+
+        currentGold = inventory.getGoldInPocket()
+
+        # Buy items
+        for purchase in buying:
+            itemId, itemQuantity = purchase
+
+            # Verify price
+            itemPrice = EconomyGlobals.getItemCost(itemId)
+            if itemPrice > currentGold:
+
+                self.air.logPotentialHacker(
+                    message='Received makeSale buy for an item the avatar can not afford!',
+                    currentGold=currentGold,
+                    itemId=itemId,
+                    itemQuantity=itemQuantity,
+                    itemPrice=itemPrice
+                )
+
+                continue
+
+            currentStack = inventory.getStack(itemId)[1]
+            inventory.b_setStack(itemId, currentStack + itemQuantity)
+            inventory.setGoldInPocket(currentGold - itemPrice)
+
+        # Sell Items
+        for sell in selling:
+            itemId, itemQuantity = sell
+            currentStack = inventory.getStack(itemId)
+
+            if currentStack < itemQuantity:
+                self.air.logPotentialHacker(
+                    message='Received makeSale sell for an item the avatar does not have!',
+                    itemId=itemId,
+                    itemQuantity=itemQuantity,
+                    currentStack=currentStack
+                )       
+
+                continue
+
+            itemPrice = EconomyGlobals.getItemCost(itemId)[1]
+            inventory.b_setStack(itemId, currentStack - itemQuantity)
+            inventory.setGoldInPocket(currentGold + itemPrice)                   
         
     def requestMusic(self, songId):
         pass
