1c9b7403f0e5cd85e6e2f0395035c30e5ca76f14
anythingtechpro <anythingtechpro@gmail.com>
2018-08-26 19:29:16 -0400

    pirates/world: code cleanup

--- pirates/world/DistributedOceanGridAI.py
+++ pirates/world/DistributedOceanGridAI.py
@@ -1,14 +1,21 @@
 from direct.distributed.DistributedCartesianGridAI import DistributedCartesianGridAI
 from direct.directnotify import DirectNotifyGlobal
+
 from pirates.world.OceanGridBase import OceanGridBase
-from pirates.world.WorldGlobals import *
+from pirates.world import WorldGlobals
+
 
 class DistributedOceanGridAI(DistributedCartesianGridAI, OceanGridBase):
     notify = DirectNotifyGlobal.directNotify.newCategory('DistributedOceanGridAI')
 
     def __init__(self, air):
-        DistributedCartesianGridAI.__init__(self, air, OCEAN_GRID_STARTING_ZONE, OCEAN_GRID_SIZE,
-            OCEAN_GRID_RADIUS, OCEAN_CELL_SIZE)
+        startingZone = WorldGlobals.OCEAN_GRID_STARTING_ZONE
+        gridRadius = WorldGlobals.OCEAN_GRID_RADIUS
+        gridSize = WorldGlobals.OCEAN_GRID_SIZE
+        cellWidth = WorldGlobals.OCEAN_CELL_SIZE
+
+        DistributedCartesianGridAI.__init__(self, air, startingZone, gridSize,
+            gridRadius, cellWidth)
 
         OceanGridBase.__init__(self)
 
