a201a4e6abc3a912cbbe50a21d17308bea8d79a2
Internet Pirate <mlgrainbowluna@gmail.com>
2018-12-10 12:32:02 -0600

    pirates/npc: forgot this

--- pirates/npc/BossNPCList.py
+++ pirates/npc/BossNPCList.py
@@ -107,7 +107,7 @@ BOSS_NPC_LIST = {
         'HpScale': 5,
         'MpScale': 3,
         'GoldScale': 2,
-        'ModelScale': 1.1000000000000001,
+        'ModelScale': 1.1,
         'DamageScale': 1.5,
         'HighlightColor': VBase3(0, 1, 0.5)},
     '1219277508.79mtucker': {
