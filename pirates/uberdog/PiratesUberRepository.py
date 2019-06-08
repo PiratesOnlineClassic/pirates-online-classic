d07021c7a8255c6bdc03d3df130d410c2404d61e
Internet Pirate <mlgrainbowluna@gmail.com>
2019-06-08 08:16:14 -0500

    pirates/uberdog: generate avatarFriendsManager

--- pirates/uberdog/PiratesUberRepository.py
+++ pirates/uberdog/PiratesUberRepository.py
@@ -41,3 +41,4 @@ class PiratesUberRepository(PiratesInternalRepository):
         self.codeRedemption = self.generateGlobalObject(OTP_DO_ID_PIRATES_CODE_REDEMPTION, 'CodeRedemption')
         self.inventoryManager = self.generateGlobalObject(OTP_DO_ID_PIRATES_INVENTORY_MANAGER, 'DistributedInventoryManager')
         self.shipLoader = self.generateGlobalObject(OTP_DO_ID_PIRATES_SHIP_MANAGER, 'DistributedShipLoader')
+        self.avatarFriendsManager = self.generateGlobalObject(OTP_DO_ID_AVATAR_FRIENDS_MANAGER, 'PCAvatarFriendsManager')
