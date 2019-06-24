c76ee3b695aa2397eef6af33f3e33d34b025969f
Caleb Marshall <anythingtechpro@gmail.com>
2019-06-24 00:30:50 -0400

    pirates/ship: formatting cleanups

--- pirates/ship/BlackPearlShipAI.py
+++ pirates/ship/BlackPearlShipAI.py
@@ -1,8 +1,7 @@
-from pirates.ship.DistributedShipAI import DistributedShipAI
 from direct.directnotify import DirectNotifyGlobal
+
+from pirates.ship.DistributedShipAI import DistributedShipAI
+
 
 class BlackPearlShipAI(DistributedShipAI):
     notify = DirectNotifyGlobal.directNotify.newCategory('BlackPearlShipAI')
-
-    def __init__(self, air):
-        DistributedShipAI.__init__(self, air)
--- pirates/ship/TutorialShipAI.py
+++ pirates/ship/TutorialShipAI.py
@@ -1,8 +1,7 @@
-from pirates.ship.DistributedShipAI import DistributedShipAI
 from direct.directnotify import DirectNotifyGlobal
+
+from pirates.ship.DistributedShipAI import DistributedShipAI
+
 
 class TutorialShipAI(DistributedShipAI):
     notify = DirectNotifyGlobal.directNotify.newCategory('TutorialShipAI')
-
-    def __init__(self, air):
-        DistributedShipAI.__init__(self, air)
