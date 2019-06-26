761015bc7029a7bdd9775496c971675c2b91ab25
Jordan Maxwell <2001527+thetestgame@users.noreply.github.com>
2019-06-26 13:09:36 -0500

    win32/start_client: Upgraded Windows batch script

--- win32/start_client.bat
+++ win32/start_client.bat
@@ -1,53 +1,109 @@
 @echo off
 title Pirates Online Classic - Client
 
-rem Server address input selection
-echo Choose your server (Default: Localhost)
-echo #1 - Localhost
-echo #2 - Development
-echo #3 - Custom
+rem User variables
+set USERPROFILE=%USERPROFILE%
 
-set INPUT=-1
-set /P INPUT=Selection: 
+rem Set Advertising values
+set GAME_SHOW_ADDS=YES
+set GAME_SHOW_FIRSTADD=0
 
-set POC_GAMESERVER=127.0.0.1
-if %INPUT%==1 (
+rem Set Website Links
+set GAME_INGAME_UPGRADE=https://www.piratesclassic.com/
+set GAME_INGAME_MOREINFO=https:/www.piratesclassic.com/about
+set GAME_INGAME_NAMING=https://www.piratesclassic.com/help/nameapprovals
+set GAME_INGAME_MANAGE_ACCT=https://www.piratesclassic.com/account
+
+rem Start operation
+goto :CHOOSESERVER
+
+:CHOOSESERVER
+    rem Server address input selection
+    echo Choose your server (Default: Localhost)
+    echo    #1 - Localhost
+    echo    #2 - Development
+    echo    #3 - Custom
+
+    set INPUT=-1
+    set /P INPUT=Selection: 
+
     set POC_GAMESERVER=127.0.0.1
-) else if %INPUT%==2 (
-    set POC_GAMESERVER=142.44.142.239
-) else if %INPUT%==3 (
-    set /P POC_GAMESERVER=Game Server IP: 
-) else (
-    set POC_GAMESERVER=127.0.0.1
-)
-title Pirates Online Classic - Client (%POC_GAMESERVER%)
+    if %INPUT%==1 (
+        set POC_GAMESERVER=127.0.0.1
+    ) else if %INPUT%==2 (
+        set POC_GAMESERVER=142.44.142.239
+    ) else if %INPUT%==3 (
+        set /P POC_GAMESERVER=Game Server IP: 
+    ) else (
+        set POC_GAMESERVER=127.0.0.1
+    )
+    title Pirates Online Classic - Client (%POC_GAMESERVER%)
 
+    goto :ENVIRONMENTTYPE
 
-rem PlayToken input
-set /P POC_TOKEN=Token (Default: dev): || ^
-set POC_TOKEN=dev
+:ENVIRONMENTTYPE
+    rem Environment input
+    set INPUT=DEV
+    set /P INPUT=Environment (LIVE, QA, TEST, DEV) (Default: DEV): 
 
-rem Choose correct python command to execute the game
-ppythona -h >nul 2>&1 && (
-    set PYTHON_CMD=ppythona
-) || (
-    set PYTHON_CMD=ppython
-)
+    for %%G in (LIVE QA TEST DEV) do (
+        if "%INPUT%"=="%%G" (
+            set GAME_ENVIRONMENT=%INPUT%
+            goto :SETENVIRONEMNT
+        )
+    )
+    goto :INVALIDENVIRONMENT
 
-rem Environment variables
-set GAME_ENVIRONMENT=DEV
+:INVALIDENVIRONMENT
+    echo %GAME_ENVIRONMENT% is not a valid environment; Default to DEV
+    set GAME_ENVIRONMENT=DEV
+    goto :SETENVIRONEMNT
 
-echo ====================================
-echo Starting Pirates Online Classic...
-echo Token: %POC_TOKEN%
-echo Gameserver: %POC_GAMESERVER%
-echo PPython: %PYTHON_CMD%
-echo ====================================
+:SETENVIRONEMNT
+    echo Client environment set to: %GAME_ENVIRONMENT%
+    title Pirates Online Classic - Client (%POC_GAMESERVER%) (%GAME_ENVIRONMENT%)
+    goto :PLAYTOKEN
 
-cd ../
+:PLAYTOKEN
+    rem PlayToken input
+    set POC_TOKEN=dev
+    set POC_TOKEN=Token (Default: dev): || ^
 
-rem Start the game using the PYTHON_CMD variable
-:main
-%PYTHON_CMD% -m pirates.piratesbase.PiratesStart
-pause
-goto :main
+    goto :FINDPYTHON
+
+:FINDPYTHON
+    rem Choose correct python command to execute the game
+    ppythona -h >nul 2>&1 && (
+        set PYTHON_CMD=ppythona
+    ) || (
+        set PYTHON_CMD=ppython
+    )
+
+    %PYTHON_CMD% -h >nul 2>&1 && (
+        goto :LAUNCH
+    ) || (
+        echo Failed to locate Panda3D python
+        pause
+        goto :EOF
+    )
+
+:LAUNCH
+
+    echo ====================================
+    echo Starting Pirates Online Classic...
+    echo Token: %POC_TOKEN%
+    echo Gameserver: %POC_GAMESERVER%
+    echo Environment: %GAME_ENVIRONMENT%
+    echo PPython: %PYTHON_CMD%
+    echo ====================================
+
+    cd ../
+    goto :RUN
+
+:RUN
+    rem Start the game using the PYTHON_CMD variable
+    %PYTHON_CMD% -m pirates.piratesbase.PiratesStart
+
+    pause
+    echo Reloading client.
+    goto :RUN
