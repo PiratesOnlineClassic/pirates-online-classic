133c77d3aaadd270b58fdfebcfdf65ae1d0fbfaa
Jordan Maxwell <2001527+thetestgame@users.noreply.github.com>
2019-07-05 09:07:15 -0500

    pirates/coderedemption: Started new version of code redemption

--- pirates/coderedemption/CodeRedemptionUD.py
+++ pirates/coderedemption/CodeRedemptionUD.py
@@ -2,6 +2,7 @@ from direct.distributed.DistributedObjectGlobalUD import DistributedObjectGlobal
 from direct.directnotify import DirectNotifyGlobal
 from pirates.uberdog.UberDogGlobals import InventoryType
 
+import json
 
 class ItemTypes:
     STACK = 1
@@ -10,69 +11,56 @@ class ItemTypes:
     HAIR_HAIR = 4
     HAIR_MUSTACHE = 5
     JEWELRY = 6
-
-
-class AwardTypes:
-    GOLD = 1
-
-    AWARD_ID = {
-        GOLD: [
-            ItemTypes.STACK,
-            InventoryType.GoldInPocket,  # Item type
-            None,
-            200,  # Amount
-            # TODO: Pull from the localizer??
-            'Gold'  # Code Name
-        ],
-    }
-
+    GOLD = 7
 
 class CodeRedemptionUD(DistributedObjectGlobalUD):
     notify = DirectNotifyGlobal.directNotify.newCategory('CodeRedemptionUD')
 
     def __init__(self, air):
         DistributedObjectGlobalUD.__init__(self, air)
+        self.verifyEndpoint = config.GetString('code-redeem-verify-url', 'https://api.piratesclassic.com/code/redeem')
 
     def announceGenerate(self):
         DistributedObjectGlobalUD.announceGenerate(self)
 
-    def getAwardFromCode(self, code):
-        for award in AwardTypes.AWARD_ID:
-            award = AwardTypes.AWARD_ID.get(award)
-            codeword = award[4].lower()
-            if codeword == code.lower():
-                return award
+    def handleVerifyCallback(self, data):
+        data = json.loads(data)
 
-    def tryRedeemCode(self, code):
+        #TODO: write this
+
+    def requestCodeVerification(self, code, username, avatar):
+        headers = {
+            'User-Agent':' UberDOG UserAgent',
+        }
+
+        body = {
+            'code': code
+            'username': username
+        }
+
+        self.air.rest.performPostRequest(
+            url=self.verifyEndpoint,
+            headers=headers,
+            content_type='application/json',
+            post_body=body,
+            callback=self.handleVerifyCallback)
+
+    def sendCodeForRedemption(self, code, username):
         avatar = self.air.doId2do.get(self.air.getAvatarIdFromSender())
 
         if not avatar:
+            self.notify.warning('Failed to request interact for non-existant avatar!')
+
+            self.air.logPotentialHacker(
+                message='Received sendCodeForRedemption from non-existant avatar',
+                targetAvId=avatar.doId,
+                doId=doId,
+                interactType=interactType,
+                instant=instant)
             return
 
-        reward = self.getAwardFromCode(code)
-
-        if reward is not None:
-            amount = reward[3]
-
-            # TODO: Rewards
-
-        else:
-            self.notify.warning('Could not redeem code; No reward found for: %s' % code)
-
-    def sendCodeForRedemption(self, code):
         code = code.lower()
-
-        avatarId = self.air.getAvatarIdFromSender()
-        response = self.tryRedeemCode(code)
-
-        reward = self.getAwardFromCode(code)
-        if reward is not None:
-            reward = reward[4].lower()
-            if code in reward:
-                self.d_notifyClientCodeRedeemStatus(avatarId, 1)
-        else:
-            self.d_notifyClientCodeRedeemStatus(avatarId, 0)
-        return response
+        self.requestCodeVerification(code, username)
 
     def d_notifyClientCodeRedeemStatus(self, avatarId, status):
         self.sendUpdateToAvatarId(avatarId, 'notifyClientCodeRedeemStatus', [status])
