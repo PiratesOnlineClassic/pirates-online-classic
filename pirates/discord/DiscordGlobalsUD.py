53a0d0ae7a57e29fd441738043d3a0f6dc1eb044
Jordan Maxwell <2001527+thetestgame@users.noreply.github.com>
2019-07-04 23:57:39 -0500

    pirates/discord: Added constant channel ids for public

--- pirates/discord/DiscordGlobalsUD.py
+++ pirates/discord/DiscordGlobalsUD.py
@@ -14,12 +14,40 @@ class DiscordChannels:
     StaffInternalReleaseNotes = 542131149066076193
     StaffUpdates = 430871490481750018
     StaffGeneral = 430822422984523779
+    StaffLeads = 430870148707254282
+    StaffLogChannel = 430877077357723648
     StaffDevelopment = 430823903846465537
+    StaffVerificationIssues = 552472982862299148
     StaffBugList = 456228201161818133
     StaffBetaChecklist = 460618982836142090
     StaffServerIssues = 436731946828300301
     StaffCrashLogs = 433429385698213888
     StaffWebDevelopment = 430874021463195648
     StaffDiscordSandbox = 431167445735178241
+    StaffBotTest = 489756496578740224
+    StaffCMNotices = 430874379996626944
+    StaffCommunications = 430874393347096576
+    StaffSeniorCM = 430882697221636100
+    StaffHackerLog = 430876132175380481
+    StaffGMNotices = 430868740037410816
+    StaffGameMasters = 430868775286603776
+    StaffSeniorGM = 430868809096757248
+
+    # Public
+    PublicReadThisFirst = 432427998038196235
+    PublicAnnouncements = 431658283284168704
+    PublicEvaluationPolls = 431658307883630593
+    PublicRulesAndGuidelines = 431658261100363776
+    PublicSneakPeaks = 433411676444950538
+    PublicProgressLog = 433411777523613696
+    PublicDevelopmentEncylopedia = 455541031874723841
+    PublicGeneral = 431519069380804608
+    PublicGMNotices = 433411250903449611
+    PublicSupport = 431938771911442442
+    PublicGuildRecruitment = 433819651366060052
+    PublicMusic = 432447685832736788
+    PublicModChat = 432002631179829258
+    PublicLogs = 431914144770424856
+    PublicTheGallows = 432306465420607490
 
 BotAuthorization = 'Bot %s' % config.GetString('discord-bot-token', '')
