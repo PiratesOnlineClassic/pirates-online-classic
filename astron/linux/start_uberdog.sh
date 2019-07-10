4f0655bb00e3973bed483dae22a8a0a3a60c0fcb
Caleb Marshall <anythingtechpro@gmail.com>
2019-07-09 22:32:03 -0400

    added linux astron startup sh files

---
+++ astron/linux/start_uberdog.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+cd ../..
+
+# Define some constants for our AI server:
+MAX_CHANNELS=999999
+STATESERVER=10000
+ASTRON_IP="127.0.0.1:7199"
+EVENTLOGGER_IP="127.0.0.1:7197"
+
+# Get the user input:
+read -p "Base channel (DEFAULT: 1000000): " BASE_CHANNEL
+BASE_CHANNEL=${BASE_CHANNEL:-1000000}
+
+echo "==============================="
+echo "Starting Pirates Online Classic UberDOG server..."
+echo "Base channel: $BASE_CHANNEL"
+echo "Max channels: $MAX_CHANNELS"
+echo "State Server: $STATESERVER"
+echo "Astron IP: $ASTRON_IP"
+echo "Event Logger IP: $EVENTLOGGER_IP"
+echo "==============================="
+
+python -m pirates.uberdog.ServiceStart --base-channel $BASE_CHANNEL \
+                 --max-channels $MAX_CHANNELS --stateserver $STATESERVER \
+                 --astron-ip $ASTRON_IP --eventlogger-ip $EVENTLOGGER_IP
