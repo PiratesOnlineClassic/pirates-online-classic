2aab19aa927e9134abee397af9e7a0b9424895a6
cmarshall108 <caleb.marshall108@gmail.com>
2020-12-03 13:56:50 -0500

    pirates/world/DistributedOceanGridAI: revert gridRadius back to the default supplied constant

--- otp/distributed/DistributedDirectoryAI.py
+++ otp/distributed/DistributedDirectoryAI.py
@@ -3,8 +3,7 @@ from direct.directnotify import DirectNotifyGlobal
 
 
 class DistributedDirectoryAI(DistributedObjectAI):
-    notify = DirectNotifyGlobal.directNotify.newCategory(
-        'DistributedDirectoryAI')
+    notify = DirectNotifyGlobal.directNotify.newCategory('DistributedDirectoryAI')
 
     def __init__(self, air):
         DistributedObjectAI.__init__(self, air)
